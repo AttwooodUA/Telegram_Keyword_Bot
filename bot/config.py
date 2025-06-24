e111d9f979d5aff79c18b1097df9976ef912f107
AttwooodUA <m.bondarchuk21@gmail.com>
2025-06-24 11:55:27 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,12 +1,5 @@
 # Bot API
-TOKEN: str = "7414576699:AAHP4eU4XQ0GkcwbmuFSFQzYVHyZOpISuBg"
-
-# User API
-PHONE_NUMBER: int = 0  # YOUR PHONE NUMBER
-API_KEY: int = 0  # YOUR API KEY
-API_HASH: str = "YOUR_API HASH_HERE"
-
-ADMIN_LIST: list = []  # User IDs which have access to bot
+BOT_TOKEN = "7414576699:AAHP4eU4XQ0GkcwbmuFSFQzYVHyZOpISuBg"
 
 # Do not set False, while Web Hook (please look at main.py) is not implemented
 DEBUG: bool = True
