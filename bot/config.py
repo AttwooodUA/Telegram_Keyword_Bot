7ef1790d878dee89859f36a0755449f9c04fa2a3
AttwooodUA <m.bondarchuk21@gmail.com>
2025-06-24 11:59:56 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,6 @@
-# Bot API
-BOT_TOKEN = "7414576699:AAHP4eU4XQ0GkcwbmuFSFQzYVHyZOpISuBg"
+import os
 
-# Do not set False, while Web Hook (please look at main.py) is not implemented
-DEBUG: bool = True
+BOT_TOKEN = os.getenv("7414576699:AAHP4eU4XQ0GkcwbmuFSFQzYVHyZOpISuBg")
+
+if not BOT_TOKEN:
+    raise ValueError("Відсутній токен Telegram бота. Встановіть змінну середовища BOT_TOKEN.")
